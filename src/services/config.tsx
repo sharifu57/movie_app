3db9e09d72e900686888b010766c5d2a9b5b6e6b
sharifu majid <sharifumajid3@gmail.com>
2024-10-16 17:45:59 +0300

    Google Login authentication

--- src/services/config.tsx
+++ src/services/config.tsx
@@ -1,4 +1,2 @@
 export const address = ''
 
-export const clientId = "846944510671-0b90i3jp6sfgb4umvirb08q1bh27vcd5.apps.googleusercontent.com"
-export const clientSecret = "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"
