3a6ff5ee986883d44865f0fc8a70e08c40ef65fb
sharifu majid <sharifumajid3@gmail.com>
2024-10-16 17:50:56 +0300

    Google Login authentication

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <GoogleOAuthProvider clientId="">
+  <GoogleOAuthProvider clientId="846944510671-0b90i3jp6sfgb4umvirb08q1bh27vcd5.apps.googleusercontent.com">
     <React.StrictMode>
       <App />
     </React.StrictMode>
